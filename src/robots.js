06d312ad4fe7b66165a6d4912217942a14309211
sulav <sulavd7@gmail.com>
2020-05-29 15:11:49 +0545

    Initial Commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'hanson robotics sophia',
+    username: 'sophia',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'mayfield robotics kuri',
+    username: 'kuri',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'sony aibo',
+    username: 'aibo',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'festo octopusgrippe',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'festo octopusgripper',
+    username: 'festo',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'honda E2-DR',
+    username: 'honda',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'piaggio gita cargo bot',
+    username: 'piaggio',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'NASA PUFFER',
+    username: 'puffer',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
